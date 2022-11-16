2451c2cdbc05abdc6edd990b64248f28b0a87dc6
Bruno Costa <ihyperbr@gmail.com>
2022-11-16 20:29:56 -0300

    Update Header.jsx

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -17,50 +17,50 @@ export function Header() {
     <header>	    
 	<nav class="desktop">
             <div><img src='' className="logo" alt='imagem'/></div>
-			<ul>
-				<li><Link className='Select' to="">&lt; Bruno Costa  /&gt;</Link></li>
-				<li><Link to="">Sobre mim</Link></li>
-				<li><Link to="">Skills</Link></li>
-				<li><Link to="">Projetos</Link></li>
-				<li><Link to="">Contato</Link></li>
-                <li><Link to="">Formação</Link></li>
-			</ul>
+		<ul>
+			<li><Link className='Select' to="">&lt; Bruno Costa  /&gt;</Link></li>
+			<li><Link to="">Sobre mim</Link></li>
+			<li><Link to="">Skills</Link></li>
+			<li><Link to="">Projetos</Link></li>
+			<li><Link to="">Contato</Link></li>
+			<li><Link to="">Formação</Link></li>
+		</ul>
 		</nav>
 
-		<div class="mobile">
-			<nav class="navbar">
-				<ul class="nav-menu">
+	<div class="mobile">
+		<nav class="navbar">
+		<ul class="nav-menu">
                     <img src={Img} className="logoMobile" alt='imagem'/>
-					<li class="nav-item">
-						<Link className='nav-link Select' to="">&lt; Bruno Costa  /&gt;</Link>
-					</li>
+			<li class="nav-item">
+				<Link className='nav-link Select' to="">&lt; Bruno Costa  /&gt;</Link>
+			</li>
 
-					<li class="nav-item">
-                        <Link className='nav-link' to="">Sobre mim</Link>
-					</li>
+			<li class="nav-item">
+                        	<Link className='nav-link' to="">Sobre mim</Link>
+			</li>
 		
-					<li class="nav-item">
-                        <Link className='nav-link' to="">Skills</Link>
-					</li>
+			<li class="nav-item">
+				<Link className='nav-link' to="">Skills</Link>
+			</li>
 
-					<li class="nav-item">
-                        <Link className='nav-link' to="">Projetos</Link>
-					</li>
+			<li class="nav-item">
+				<Link className='nav-link' to="">Projetos</Link>
+			</li>
 		
-					<li class="nav-item">
-                        <Link className='nav-link' to="">Contato</Link>
-					</li>
-                    <li class="nav-item">
-                        <Link className='nav-link' to="">Formação</Link>
-					</li>
-				</ul>
+			<li class="nav-item">
+				<Link className='nav-link' to="">Contato</Link>
+			</li>
+                    	<li class="nav-item">
+                        	<Link className='nav-link' to="">Formação</Link>
+			</li>
+		</ul>
 		
                 <button onClick={menuHamburger}>
                     <div class="hamburger" id="menuHamburger">
                         <span class="bar"></span>
                         <span class="bar"></span>
                         <span class="bar"></span>
-                    </div>     
+                   </div>     
                 </button>                                       
             </nav>
 	</div>
